4af0fef44abbab546709c6db5d21faed95ab02ee
Nishant choudhary <65034464+Nishant-001@users.noreply.github.com>
2023-05-18 11:32:51 +0530

    Update SignupController.java

--- src/main/java/com/ecommerce/controller/SignupController.java
+++ src/main/java/com/ecommerce/controller/SignupController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.ecommerce.dto.SignupRequest;
 import com.ecommerce.entity.User;
